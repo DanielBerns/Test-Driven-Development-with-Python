823c9b875310ea9f0196a5ef44ba9282a52c0411
Freddie Karlbom <freddiekarlbom@gmail.com>
2018-08-30 15:38:07 +0200

    Adding new functional test for layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -103,3 +103,23 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # find css tag and div box tag, check that class name is correct?
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] +
+                               inputbox.size['width'] / 2,
+                               512,
+                               delta=10)
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.size['width'] / 2,
+            512,
+            delta=10)
